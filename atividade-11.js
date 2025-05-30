9730cb9d260eb2995f96222a8360edc92b3f3c2a
Lucas <lucastavaresbento@gmail.com>
2025-05-30 11:56:09 -0300

    Cria arquivo atividade-11 com códigos JavaScript

---
+++ atividade-11.js
@@ -0,0 +1,13 @@
+/* 11 – Geração de frase
+Você precisa exibir os ingredientes de uma receita em uma única frase legível para o cliente. Utilize o array de ingredientes.
+*/
+let ingredientes = [
+  "farinha de trigo",
+  "fermento",
+  "leite",
+  "ovos",
+  "açúcar",
+  "manteiga"
+];
+
+console.log(`Lista de igredientes organizado: ${ingredientes.join(',')}`);
