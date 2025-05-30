efb5cec4f0fcfaf28c941a3a3d66928567eb3227
Lucas <lucastavaresbento@gmail.com>
2025-05-30 11:43:53 -0300

    Cria arquivo atividade-08 com códigos JavaScript

---
+++ atividade-08.js
@@ -0,0 +1,14 @@
+/*  8 – Alunos transferidos
+Duas alunas transferidas chegaram atrasadas e devem ser colocadas no início da lista de chamada. Os nomes são "Beatriz" e "Rafaela".
+*/
+let chamada = [
+  "Carlos",
+  "Fernanda",
+  "João",
+  "Mariana",
+  "Lucas",
+  "Camila"
+];
+
+chamada.unshift("Beatriz","Rafaela");
+console.log("Lista de chamada:", chamada);
