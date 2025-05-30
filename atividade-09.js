069123e5d64be65e78b57f9c191767de2173d90a
Lucas <lucastavaresbento@gmail.com>
2025-05-30 11:47:56 -0300

    Cria arquivo atividade-09 com códigos JavaScript

---
+++ atividade-09.js
@@ -0,0 +1,13 @@
+/*  9 – Conferência de presença
+O RH quer saber se o funcionário "Amanda Souza" compareceu à reunião de alinhamento. Verifique essa informação na lista de presença.
+*/
+
+let presenca = [
+  "João Lima",
+  "Carla Nunes",
+  "Amanda Souza",
+  "Bruno Ribeiro",
+  "Fernanda Sales"
+];
+
+console.log(`A funcionária "Amanda Souza" compareceu?`, presenca.includes('Amanda Souza'))
