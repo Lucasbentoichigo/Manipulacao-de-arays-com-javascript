20aa203de7714148843e5351be318997396d4cad
Lucas <lucastavaresbento@gmail.com>
2025-05-30 11:13:43 -0300

    Cria arquivo atividade-02 com códigos JavaScript

--- atividade-02.js
+++ atividade-02.js
@@ -1,1 +1,16 @@
+/*  2 – Expansão de cardápio
+Uma lanchonete digital possui um menu com pratos principais. O chef pediu que fossem adicionados dois novos itens no final da lista para a próxima semana. Os itens são: "hambúrguer artesanal" e "wrap de frango". Atualize o cardápio.
+*/
+let cardapio = [
+  "pizza de calabresa",
+  "lasanha",
+  "sushi",
+  "salada Caesar",
+  "pastel",
+  "yakisoba",
+  "feijoada"
+];
 
+cardapio.push("hambúrguer artesanal");
+cardapio.push("wrap de frango")
+console.log(`Cardápio atualizado: ${cardapio}`);
